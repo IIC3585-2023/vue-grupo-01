12f0ae394b47e6fb03a76cd4e9665d79cc04dee0
Benjamín Vicente <benjavicente@uc.cl>
2023-06-01 23:25:30 -0400

    hmmm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxtjs/eslint-module',
-    '@nuxtjs/tailwindcss',
-    '@pinia/nuxt'
-  ],
+  modules: ['@nuxtjs/eslint-module', '@nuxtjs/tailwindcss', '@pinia/nuxt'],
   runtimeConfig: {
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
     public: {
-      VERCEL_URL: `https://${process.env.VERCEL_URL}/api/auth/callback`,
+      VERCEL_URL: `https://${process.env.HOST ?? process.env.VERCEL_URL}/api/auth/callback`,
       GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-      GITHUB_REDIRECT_URI: process.env.GITHUB_REDIRECT_URI
-    }
+      GITHUB_REDIRECT_URI: process.env.GITHUB_REDIRECT_URI,
+    },
   },
   eslint: {
-    lintOnStart: false
-  }
+    lintOnStart: false,
+  },
 })
